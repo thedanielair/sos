f14d3cf73d340076d49560a9cd5ad88ba5d97a92
Danila Mikheev <thedanielair@mail.ru>
2025-04-21 15:39:49 +0300

    Optimize TSX icons, Create global styles & utils

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    prependData: `@import "@/styles/utils/variables.scss";`,
+  },
 };
 
 export default nextConfig;
